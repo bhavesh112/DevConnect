d2dff210a1a43f474123cd76b48462a45e3ecf6b
Bhavesh Wadhwani <bhaveshwadhwani21@gmail.com>
2021-07-18 10:29:34 +0530

    Resolved github profile issue

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -7,41 +7,33 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   useEffect(() => {
     getGithubRepos(username);
   }, [getGithubRepos, username]);
-
+  console.log(repos);
   return (
     <div className="profile-github">
       <h2 className="text-primary my-1">Github Repos</h2>
-      {repos.length ? (
-        repos.map((repo) => (
-          <div key={repo.id} className="repo bg-white p-1 my-1">
-            <div>
-              <h4>
-                <a
-                  href={repo.html_url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {repo.name}
-                </a>
-              </h4>
-              <p>{repo.description}</p>
-            </div>
-            <div>
-              <ul>
-                <li className="badge badge-primary">
-                  Stars: {repo.stargazers_count}
-                </li>
-                <li className="badge badge-dark">
-                  Watchers: {repo.watchers_count}
-                </li>
-                <li className="badge badge-light">Forks: {repo.forks_count}</li>
-              </ul>
-            </div>
+      {repos.map((repo) => (
+        <div key={repo.id} className="repo bg-white p-1 my-1">
+          <div>
+            <h4>
+              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                {repo.name}
+              </a>
+            </h4>
+            <p>{repo.description}</p>
           </div>
-        ))
-      ) : (
-        <></>
-      )}
+          <div>
+            <ul>
+              <li className="badge badge-primary">
+                Stars: {repo.stargazers_count}
+              </li>
+              <li className="badge badge-dark">
+                Watchers: {repo.watchers_count}
+              </li>
+              <li className="badge badge-light">Forks: {repo.forks_count}</li>
+            </ul>
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
