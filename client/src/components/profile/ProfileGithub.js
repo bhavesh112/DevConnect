67748e035f84bdcba2ea7f2fefc15c7983f7fc14
Bhavesh Wadhwani <bhaveshwadhwani21@gmail.com>
2021-07-18 10:19:07 +0530

    Resolved github profile issue

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -11,29 +11,37 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
   return (
     <div className="profile-github">
       <h2 className="text-primary my-1">Github Repos</h2>
-      {repos.map(repo => (
-        <div key={repo.id} className="repo bg-white p-1 my-1">
-          <div>
-            <h4>
-              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-                {repo.name}
-              </a>
-            </h4>
-            <p>{repo.description}</p>
+      {repos.length ? (
+        repos.map((repo) => (
+          <div key={repo.id} className="repo bg-white p-1 my-1">
+            <div>
+              <h4>
+                <a
+                  href={repo.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {repo.name}
+                </a>
+              </h4>
+              <p>{repo.description}</p>
+            </div>
+            <div>
+              <ul>
+                <li className="badge badge-primary">
+                  Stars: {repo.stargazers_count}
+                </li>
+                <li className="badge badge-dark">
+                  Watchers: {repo.watchers_count}
+                </li>
+                <li className="badge badge-light">Forks: {repo.forks_count}</li>
+              </ul>
+            </div>
           </div>
-          <div>
-            <ul>
-              <li className="badge badge-primary">
-                Stars: {repo.stargazers_count}
-              </li>
-              <li className="badge badge-dark">
-                Watchers: {repo.watchers_count}
-              </li>
-              <li className="badge badge-light">Forks: {repo.forks_count}</li>
-            </ul>
-          </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
@@ -44,7 +52,7 @@ ProfileGithub.propTypes = {
   username: PropTypes.string.isRequired
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   repos: state.profile.repos
 });
 
